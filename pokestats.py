7c0ce3b22170f906126b1b5136ca718bf7b2f37d
S1moxi11 <simonmarie055@gmail.com>
2024-12-03 16:15:25 +0100

    third commit

--- pokestats.py
+++ pokestats.py
@@ -1,6 +1,7 @@
 import requests
 import math
 import heapq
+from utils.download_cache import *
 
 def req(ch:str):
     return requests.get(ch).json()
@@ -13,24 +14,27 @@ dracofeu = req("https://pokeapi.co/api/v2/pokemon/charizard")
 """
 
 def max_pc(level=100):
-    dico={}
-    nom,sta,vhp,vat,vdef=0,0,0,0,0
-    for i in req("https://pokeapi.co/api/v2/pokemon/?limit=1302")["results"]:
-        for keys, val in req(i["url"]).items():
-            if keys == "forms" :
-                nom = val[0]["name"]
-            if keys == "stats" :
-                for i in val:
-                    if i["stat"]["name"] == "hp":
-                        vhp = i["base_stat"]
-                    elif i["stat"]["name"] == "attack":
-                        vat = i["base_stat"]
-                    elif i["stat"]["name"] == "defense":
-                        vdef = i["base_stat"]
-                sta = (0.01 * math.sqrt(vhp * vat * vdef)) * level
-        dico[nom]=round(sta)
+    dico = {}
+    
+    for i in download("https://pokeapi.co/api/v2/pokemon/?limit=1302")["results"]:
+        nom, sta, vhp, vat, vdef = "", 0, 0, 0, 0
+        
+        poke_data = download_poke_cached(i["url"].split("/")[-2])
+        nom = poke_data["name"]
+        
+        for stats in poke_data["stats"]:
+            if stats["stat"]["name"] == "hp":
+                vhp = stats["base_stat"]
+            elif stats["stat"]["name"] == "attack":
+                vat = stats["base_stat"]
+            elif stats["stat"]["name"] == "defense":
+                vdef = stats["base_stat"]
 
+        sta = (0.01 * math.sqrt(vhp * vat * vdef)) * level
+        dico[nom] = round(sta)
+    
     return dico
+
 
 """print(max_pc())"""
 
