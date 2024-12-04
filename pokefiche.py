cceccc4d9dd88469770ab58a3ee5e124fea7e49c
S1moxi11 <simonmarie055@gmail.com>
2024-12-04 14:24:04 +0100

    first commit

---
+++ pokefiche.py
@@ -0,0 +1,33 @@
+import requests
+from utils.download_cache import *
+
+id_choice=input("Entrez l'id ou le nom en anglais du pokémon sur lequel vous souhaitez avoir des informations : ")
+
+def req(ch:str):
+    return requests.get(ch).json()
+
+def stats_poke(id):
+    data=download_poke_cached(id)
+    dico={}
+    dico["height"]=data["height"]
+    dico["weight"]=data["weight"]
+    for stats in data["stats"]:
+        if stats["stat"]["name"] == "hp":
+            dico["hp"] = stats["base_stat"]
+        elif stats["stat"]["name"] == "attack":
+            dico["attack"] = stats["base_stat"]
+        elif stats["stat"]["name"] == "defense":
+            dico["defense"] = stats["base_stat"]
+        elif stats["stat"]["name"] == "speed":
+            dico["speed"] = stats["base_stat"]
+    nb=1
+    for i in data["types"]:
+        dico["type "+str(nb)]=i["type"]["name"]
+        nb+=1
+    print('\nVoici les informations sur '+data["forms"][0]["name"]+" :")
+    return dico
+
+
+"""print(stats_poke(646))"""
+print(stats_poke(id_choice))
+
