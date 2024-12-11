1edf9400243e15e3d7510905f8c06f4b5f797bb5
S1moxi11 <simonmarie055@gmail.com>
2024-12-11 13:07:26 +0100

    ameliorations

--- utils/trad.py
+++ utils/trad.py
@@ -1,16 +1,8 @@
 import requests
 
 
-def name_trad(url: str) -> str:
+def trad(url: str) -> str:
     t=requests.get(url).json()
-    f=requests.get(t["species"]["url"]).json()
-    for i in f["names"]:
-        if i["language"]["name"]=="fr":
-            return i["name"]
-
-def type_trad(url: str) -> str:
-    t=requests.get(url).json()
-    f=requests.get(t["types"][0]["type"]["url"]).json()
-    for i in f["names"]:
+    for i in t["names"]:
         if i["language"]["name"]=="fr":
             return i["name"]
