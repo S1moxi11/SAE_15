4cfa1355b485aafdce2ed7d716430d0ebf548e01
S1moxi11 <simonmarie055@gmail.com>
2024-12-11 14:55:54 +0100

    trads

--- pokestats.py
+++ pokestats.py
@@ -2,12 +2,21 @@ import requests
 import math
 import heapq
 from utils.download_cache import *
+from utils.trad import *
+import webbrowser
 
 def req(ch:str):
     return requests.get(ch).json()
 
+"""def get_dataset():
+    L=[]
+    for i in download("https://pokeapi.co/api/v2/pokemon/?limit=1302")["results"]:
+        L.append(download_poke_cached(i["url"].split("/")[-2]))"""
 
-def max_pc(level=100):
+
+def compute_statistics(level=100):
+    """Cette fonction renvoie un dictionnaire avec 
+    le nom du pokemon en clé ainsi ses pc en valeur pour tous les pokémons"""
     dico = {}
     
     for i in download("https://pokeapi.co/api/v2/pokemon/?limit=1302")["results"]:
@@ -29,22 +38,54 @@ def max_pc(level=100):
     
     return dico
 
+"""print(compute_statistics)"""
 
-"""print(max_pc())"""
-top_10 = heapq.nlargest(10, max_pc().items(), key=lambda x: x[1])
+top_10 = heapq.nlargest(10, compute_statistics().items(), key=lambda x: x[1])
 """print(top_10)"""
+
+def trad_list(L):
+    nvL=[]
+    for i in L:
+        nvL.append(((trad(req("https://pokeapi.co/api/v2/pokemon/"+i[0])["species"]["url"]),i[0]),i[1]))
+    return nvL
+    
+"""print(trad_list(top_10))"""
 
 def img(L):
     dico={}
     nrml=None
     shiny=None
     for i in L:
-        for keys, val in req("https://pokeapi.co/api/v2/pokemon-form/"+i[0])["sprites"].items():
+        for keys, val in req("https://pokeapi.co/api/v2/pokemon-form/"+i[0][1])["sprites"].items():
             if keys == "front_default":
                 nrml=val
             elif keys == "front_shiny":
                 shiny=val
-            dico[i[0]]=[nrml, shiny]
+            dico[i[0][0]]=[nrml, shiny]
     return dico
 
-print(img(top_10))
+
+print(img(trad_list(top_10)))
+
+
+def types_pokemons():
+    pass
+
+def moyenne_types():
+    pass
+
+
+def poke_to_md(data: dict, filename: str) -> None:
+    # A completer
+
+    with open(filename,'w') as f:
+        f.write("A completer")
+    pass
+
+
+"""webbrowser.open("page_poke.html")"""
+
+
+
+"""Donner la moyenne des pc des pokemons 
+ayant le plus de pcs en fonction de leurs types"""
