9e99cc3b7be9765d661dbfeb1700f1d758c8d3cf
S1moxi11 <simonmarie055@gmail.com>
2024-12-20 11:52:26 +0100

    commit 20/12

--- utils/download_cache.py
+++ utils/download_cache.py
@@ -7,17 +7,17 @@ def download(url: str) -> dict:
     dl.raise_for_status()
     return dl.json()
 
-def download_poke_cached(id: int) -> dict:
+def download_poke_cached(nom: str) -> dict:
     if not os.path.isdir('cache'):
         os.mkdir("cache")
-    cache_path= f"cache/{id}.json"
+    cache_path= f"cache/{nom}.json"
     
     if os.path.isfile(cache_path):
         with open(cache_path,"r") as f:
             f_data = json.load(f)
         return f_data
     else:
-        url = f"https://pokeapi.co/api/v2/pokemon/{id}/"
+        url = f"https://pokeapi.co/api/v2/pokemon/{nom}/"
         f_data=download(url)
         f = open(cache_path,"w")
         json.dump(f_data,f)
