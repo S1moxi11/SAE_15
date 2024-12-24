b5a50f22a818117e30f4f16777557bf0e12f33f4
S1moxi11 <simonmarie055@gmail.com>
2024-12-24 16:59:11 +0100

    Style page

--- utils/md_to_html.py
+++ utils/md_to_html.py
@@ -1,9 +1,22 @@
 import markdown
 
-def convert(md,html_file):
-    with open(md,'r') as f:
-        text=f.read()
+def convert(md, html_file):
+    with open(md, 'r') as f:
+        text = f.read()
     html = markdown.markdown(text)
 
-    with open(html_file, 'w') as f:
-        f.write(html)
+    full_html = f"""
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Statistiques des Pokémon</title>
+        <link rel="stylesheet" href="pokemon_styles.css">
+    </head>
+    <body>
+        {html}
+    </body>
+    </html>
+    """
+    with open(html_file, 'w', encoding='utf-8') as f:
+        f.write(full_html)
